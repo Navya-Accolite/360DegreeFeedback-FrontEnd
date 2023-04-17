50b16310d8dcee014159a7d5b7fe927b1c657810
Nettem Lakshmi <nettem.lakshmi@accolite.com>
2023-04-17 11:02:40 +0530

    select button changed

--- src/components/ManageUsers.js
+++ src/components/ManageUsers.js
@@ -1,92 +1,129 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Form,FormGroup,Label,Row,Col,Input} from 'reactstrap';
-import {Button} from 'reactstrap';
-
+import { Form, FormGroup, Label, Row, Col, Input } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 function ManageUsers() {
-  
-  const [email,updateemail]=useState("");
-  const [role,updaterole]=useState("");
+  const [email, updateEmail] = useState('');
+  const [role, updateRole] = useState('');
+  const [showOptions, setShowOptions] = useState(false);
 
-    function updateuser(){  
-
-      const url="http://localhost:4545/api/update/"+email;
-      //  console.log(role);
-       const postdata={
-          "role":role
-       }
-       axios.put(url, postdata, { headers: { 'Content-Type': 'application/json' }})
-       .then(response => {
-        //  console.log(response.data);
-       })
-       .catch(error => {
-        //  console.error(error);
-       });
-    }
+  function updateuser() {
+    const url = `http://localhost:4545/api/update/${email}`;
+    const postdata = {
+      role: role,
+    };
+    axios
+      .put(url, postdata, { headers: { 'Content-Type': 'application/json' } })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   return (
     <>
-    <div>
-      <h3>Manage your Users!</h3>
-    </div>
+      <div>
+        <h3>Manage your Users!</h3>
+      </div>
 
+      <div className="manageusers">
+        <div>
+          <div>
+            <Label for="exampleEmail" sm={2}>
+              Email
+            </Label>
+            <Col sm={10}>
+              <Input
+                id="exampleEmail"
+                name="email"
+                placeholder="with a placeholder"
+                type="email"
+                onChange={(e) => updateEmail(e.target.value)}
+              />
+            </Col>
+          </div>
 
-    <div className='manageusers'>
-   <div>
-   <div>
-    <Label
-      for="exampleEmail"
-      sm={2}
-    >
-      Email
-    </Label>
-    <Col sm={10}>
-      <Input
-        id="exampleEmail"
-        name="email"
-        placeholder="with a placeholder"
-        type="email"
-        onChange={(e)=>updateemail(e.target.value)}
-       
-      />
-    </Col>
-  </div>
-
-  <div>
-    <Label
-      for="exampleSelect"
-      sm={2}
-    >
-      Select
-    </Label>
-    <Col sm={10}>
-      <Input
-        id="exampleSelect"
-        name="select"
-        type="select"
-        onChange={(e)=>updaterole(e.target.value)}
-      >
-        <option>
-          ADMIN
-        </option>
-        <option>
-          USER
-        </option>
-      </Input>
-    </Col>
-  </div>
-  <button id='btn4' onClick={()=>updateuser()}>Update</button>
-
-   </div>
-    </div>
-    
+          <div>
+            <Label for="exampleSelect" sm={2}>
+              Select
+            </Label>
+            <Col sm={10}>
+              <div
+                style={{
+                  border: '1px solid #ced4da',
+                  borderRadius: '0.25rem',
+                  padding: '0.375rem 0.75rem',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  cursor: 'pointer',
+                }}
+                onClick={() => setShowOptions(!showOptions)}
+              >
+                <div>{role ? role : 'Select'}</div>
+                <div
+                  style={{
+                    borderLeft: '1px solid #ced4da',
+                    paddingLeft: '0.5rem',
+                  }}
+                >
+                  <span
+                    className={
+                      showOptions
+                        ? 'fas fa-chevron-up'
+                        : 'fas fa-chevron-down'
+                    }
+                  ></span>
+                </div>
+              </div>
+              {showOptions && (
+                <div
+                  style={{
+                    position: 'absolute',
+                    backgroundColor: '#fff',
+                    border: '1px solid #ced4da',
+                    borderRadius: '0.25rem',
+                    width: '100%',
+                    marginTop: '0.5rem',
+                  }}
+                >
+                  <option
+                    style={{ padding: '0.375rem 0.75rem' ,
+                            cursor:'pointer' 
+                  }}
+                    value="ADMIN"
+                    onClick={() => {
+                      updateRole('ADMIN');
+                      setShowOptions(false);
+                    }}
+                  >
+                    ADMIN
+                  </option>
+                  <option
+                    style={{ padding: '0.375rem 0.75rem' ,
+                    cursor: 'pointer',}}
+                    value="USER"
+                    onClick={() => {
+                      updateRole('USER');
+                      setShowOptions(false);
+                    }}
+                  >
+                    USER
+                  </option>
+                </div>
+              )}
+            </Col>
+          </div>
+          <button id="btn4" onClick={() => updateuser()}>
+            Update
+          </button>
+        </div>
+      </div>
     </>
-
-  )
+  );
 }
 
-export default ManageUsers
-
-
-
+export default ManageUsers;
