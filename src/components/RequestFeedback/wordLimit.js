5da860be385e46bb221bebe1f4fa0607542703f6
Pratyush <pratyush.singh@accolitedigital.com>
2023-04-28 15:44:26 +0530

    Word Limit

--- src/components/RequestFeedback/wordLimit.js
+++
@@ -1,9 +0,0 @@
-var myText = documnet.getElementBYId("my-text");
-var res = document.getElementById("result");
-var limit = 1000;
-result.textContent = 0 + "/" + limit;
-
-NoStyleItemContext.addEventListner("input",function(){
-    var textLength = myText.value.length;
-    console.log(textLength);
-});
